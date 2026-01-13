9f18cce6519890d1f43ad898457546631dd5e7c0
daria <dariavesnyak@gmail.com>
2026-01-13 19:01:50 +0300

    commented string with configuration remote

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -18,7 +18,7 @@ public class TestBase {
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
         Configuration.baseUrl = "https://demoqa.com";
-       Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+       //Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
