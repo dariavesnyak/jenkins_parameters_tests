7af807ba9fa23d15160d730e897c00ad35e87cdd
daria <dariavesnyak@gmail.com>
2026-01-14 13:59:46 +0300

    added configuration for setting browser

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -15,12 +15,14 @@ public class TestBase {
 
     @BeforeAll
     static void beforeAll() {
-        Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.remote= System.getProperty("remote");
+        Configuration.browser= System.getProperty("browser", "chrome");
+        Configuration.browserVersion= System.getProperty("browserVersion", "128.0");
+        Configuration.browserSize= System.getProperty("browserSize", "1920x1080");
 
-       //Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
